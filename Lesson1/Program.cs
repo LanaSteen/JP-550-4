6eb4d9ffdbc53ee893a17ee01805877b12456eab
LanaNew <l4n.steen@gmail.com>
2026-09-22 17:55:26 +0400

    first lessson done

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -4,9 +4,78 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			//Console.WriteLine("Hello, World!");
 
-			Console.WriteLine(	"hi");
+			//Console.WriteLine(	"hi");
+			//Console.WriteLine('a');
+
+
+
+			//int score = int.Parse(Console.ReadLine());
+
+			//byte age =byte.Parse(Console.ReadLine());
+
+			//double avgScore = double.Parse(Console.ReadLine());
+
+			//var x = 10;
+
+			//var y = 5.3;
+
+			//int rame;  // 0
+			//var rgac = 20;
+			//9.5
+
+
+			// კონსოლში დაბეჭდეთ კითხვა : "შეიყვანეთ თქვენი ასაკი" და შემდეგ
+			// შეინახეთ მომხმარებლის მიერ შეყვანილი მნიშვნელობა age ცვლადში.
+			// შემდეგ დაბეჭდეთ age ცვლადის მნიშვნელობა კონსოლში.
+
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.InputEncoding = System.Text.Encoding.UTF8;
+			Console.Write("შეიყვანეთ ასაკი - ");
+
+
+
+			byte age = byte.Parse(Console.ReadLine());
+
+			Console.WriteLine("თქვენი ასაკი: " + age);
+			Console.WriteLine($"თქვენი ასაკი: {age} ");
+
+
+			//`${ }`
+
+
 		}
 	}
 }
+//"20"
+
+//oop
+
+//მეთოდი და ფუნქცია
+
+//alert()  // function
+
+//Console.log()  // method
+
+
+//""  ''  ``
+//string 
+//boolean
+//objext
+
+
+//"a" 'dsdsd'
+
+
+// char    'a'   
+// string  ""
+
+// bool
+
+
+//number 
+
+
+
+
